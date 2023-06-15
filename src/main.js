b574ca9a6b28ee5801edec88082fbd89e5f6d41c
mahmutyoung <mahmutyoung@gmail.com>
2023-06-15 23:52:53 +0300

    working on it

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 600;
-unit = canvas.width/20;
+const unit = canvas.width / 20;
 
 class Tetromino {
   /* constructor(shapePath, type, color, canMove) {
@@ -11,46 +12,80 @@ class Tetromino {
     this.canMove = canMove;
   }
   */
-  constructor(canvas, x, y, width, height, color, velocity) {
-    this.width = width;
+  constructor(canvas, x, y, width, height, color, vx, vy) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
     this.x = x;
     this.y = y;
+    this.width = width;
     this.height = height;
-    this.canvas = canvas;
     this.color = color;
-    this.velocity = velocity;
-    this.ctx = canvas.getContext("2d");
+    this.vx = vx;
+    this.vy = vy;
   }
 
   draw() {
     this.ctx.fillStyle = this.color;
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
-    console.log(this.ctx.getImageData(this.x, this.y, 1, 1).data);
+    this.ctx.font = "16px Arial";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText("L", this.x + 10, this.y + 15);
+    //console.log(this.ctx.getImageData(this.x, this.y, 1, 1).data);
   }
-  rotateLShape() {
-    this.ctx.translate(50, 50);
-    this.ctx.rotate((90 * Math.PI) / 180);
-    this.ctx.translate(-50, -50);
-    this.ctx.fillStyle = "red";
-    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  rotateShape() {
+    let newCoordinates = {
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height,
+    };
+    this.x -= newCoordinates.height;
+    this.y -= newCoordinates.width;
+    this.width = newCoordinates.height;
+    this.height = newCoordinates.width;
+    //this.ctx.fillStyle = "red";
+    //this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    console.log(myTetromino);
   }
   update() {
-    this.y += this.velocity.y;
+    if (this.y < canvas.height - this.height) {
+      this.y += this.vy;
+    }
+
+    console.log(this.y);
   }
 }
 
-const myTetromino = new Tetromino(canvas, 10*unit, 3*unit,unit, unit*4, "red");
+const myTetromino = new Tetromino(
+  canvas,
+  10 * unit,
+  3 * unit,
+  unit,
+  unit * 4,
+  "red",
+  0,
+  1
+);
 
 myTetromino.draw();
+console.log(myTetromino);
 
-document.addEventListener(
-  "keydown",
-  (event) => {
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    if (event.code === "ArrowUp") {
-      event.preventDefault();
-      myTetromino.rotateShape();
-    }
-  },
-  { capture: true }
-);
+document.addEventListener("keydown", (event) => {
+  //canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  if (event.code === "ArrowUp") {
+    event.preventDefault();
+    myTetromino.rotateShape();
+  }
+});
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  myTetromino.draw();
+  myTetromino.update();
+
+  console.log("game loop");
+  console.log(myTetromino);
+  requestAnimationFrame(gameLoop);
+}
+
+//gameLoop();
